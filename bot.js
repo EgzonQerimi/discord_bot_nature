ba5e8752f0bb78bed05ffa2b471c3d112fb78bba
Egzon Qerimi <egzonqerimi@outlook.com>
2026-04-22 23:11:58 +0200

    cleaned the files

---
+++ bot.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const axios = require("axios");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const WEBHOOK_URL =
+  "https://cupped-mumble-groin.ngrok-free.dev/webhook/discord-nature";
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.mentions.has(client.user)) {
+    try {
+      await axios.post(WEBHOOK_URL, {
+        content: message.content,
+        channel_id: message.channel.id,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+client.login(process.env.API_KEY);
